49eb3d4c860646fcf688addaf4dc953aa4f01228
AntonioSemaan <antonio.semaan@isae.edu.lb>
2019-06-13 07:04:02 +0300

    question 2 manquante

--- question1/SoldeDebiteurException.java
+++
@@ -1,6 +0,0 @@
-package question1;
-
-public class SoldeDebiteurException extends Exception{
-  public SoldeDebiteurException(){ super();}
-  public SoldeDebiteurException(String message){ super(message);}
-}
--- question1/Visiteur.java
+++
@@ -1,8 +0,0 @@
-package question1;
-
-public abstract interface Visiteur<T> {
-    
-    public abstract T visite(Contributeur c);
-    public abstract T visite(GroupeDeContributeurs g);
-    
-}
